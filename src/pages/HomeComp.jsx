c299969f8526c9c1239bfbdd4f0191c00209e632
Khush Patel <202001127@daiict.ac.in>
2023-07-20 01:51:45 +0530

    go to feature added

--- src/pages/HomeComp.jsx
+++ src/pages/HomeComp.jsx
@@ -21,6 +21,37 @@ const HomeComp = () => {
     }
   };
 
+  const getIdByName = (ers,Name) => {
+
+    console.log("this is  : ",Name);
+    for(let i = 0; i < ers.length;i++)
+      {
+        console.log(ers[i].name);
+        if(Name.name === ers[i].name){
+          
+            return ers[i]._id;
+        }
+      }
+      return 0;
+  }
+  const handleGotoER = async () => {
+    try {
+      const res = await axios.get("http://localhost:8080/api/getAll"); // Get all ER diagrams
+      const ers = res.data;
+      // getIdByName
+      console.log(ers);
+      console.log(name);
+      const newId = getIdByName(ers,name);
+      if(newId === 0){
+         alert("their is no ER-Diagram present with this name.Please check the name")
+      }else{
+        navigate(`${newId}`); // Redirect to first ER diagram page
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="home">
       <div className="container">
@@ -38,6 +69,12 @@ const HomeComp = () => {
           </button>
           <p className="instructions">
             Click the button above to create a new ER diagram.
+          </p>
+          <button className="btn" onClick={handleGotoER}>
+            GO to ER Diagram
+          </button>
+          <p className="instructions">
+            Click the button above to open exisiting ER diagram.
           </p>
         </div>
       </div>
